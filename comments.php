50d0e1135fe322ee55c16eb061cf3504eee5938e
far1h <farihmuhammad45@gmail.com>
2024-09-14 17:18:50 +0700

    Add course files

---
+++ comments.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+        /* This function below displays text
+
+        this is multiline comment */
+
+        // One line comment
+
+        echo "Hello Student";
+
+    ?>
+
+</body>
+</html>
