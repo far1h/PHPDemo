9443ac6b3d2ec240a9aaaec5838d1fce93d1f7e8
far1h <farihmuhammad45@gmail.com>
2024-09-14 17:20:25 +0700

    Complete Practice Section 1

--- _practical-app/1.php
+++ _practical-app/1.php
@@ -17,10 +17,12 @@
 
 		<?php  
 
+		// This function says hello
+		echo "<h1>Hello</h1>";
+	
 
 		/* 
 		   Step 1:  Use the Echo Function to say hello with html h1 tags embedded inside php.
-			
 
 		   Step 2: Write a comment above the echo function and explain
 		   what that function is doing.
