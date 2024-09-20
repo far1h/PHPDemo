5993b8f60f32f67e94454feb35aafd1b9f309819
far1h <farihmuhammad45@gmail.com>
2024-09-20 08:16:21 +0700

    Add variables

---
+++ basics/variables.php
@@ -0,0 +1,18 @@
+<?php
+
+$name;
+
+$name = "Nigga";
+$number = 2;
+
+$name = "Jose";
+
+$NAME = "Jose";
+
+// $0name = "RICO"; WRONG
+// $full-name = "DWIA"; WRONG
+
+$full_name = "Rico"; // Snake case (most common)
+$fullName = "Drake"; // camel case
+$FullName = "CHICO"; // pascal case
+var_dump($name);
