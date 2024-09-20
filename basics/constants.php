2391f1e50817001a1acb160fc1cd103bb02dafd5
far1h <farihmuhammad45@gmail.com>
2024-09-20 08:36:49 +0700

    Add magic constants

--- basics/constants.php
+++ basics/constants.php
@@ -1,7 +1,13 @@
 <?php
 
-define("DB_PASS", "secret123@123");
+// define("DB_PASS", "secret123@123");
 
-const DB_PASS = "DHWAIOD";
+// const DB_PASS = "DHWAIOD";
 
-echo DB_PASS; 
+// echo DB_PASS; 
+
+// magic constants
+
+echo __DIR__;
+echo "<br>";
+echo __FILE__;
