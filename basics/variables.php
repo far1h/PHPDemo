24d2b74fec8c17e248d1e73fb417f6debd84cb9e
far1h <farihmuhammad45@gmail.com>
2024-09-20 08:20:24 +0700

    Add variables pt 2

--- basics/variables.php
+++ basics/variables.php
@@ -4,6 +4,7 @@ $name;
 
 $name = "Nigga";
 $number = 2;
+$float = 2.5;
 
 $name = "Jose";
 
@@ -12,7 +13,14 @@ $NAME = "Jose";
 // $0name = "RICO"; WRONG
 // $full-name = "DWIA"; WRONG
 
-$full_name = "Rico"; // Snake case (most common)
-$fullName = "Drake"; // camel case
+$full_name = "Rico"; // Snake case 
+$fullName = "Drake"; // camel case (most common)
 $FullName = "CHICO"; // pascal case
-var_dump($name);
+// var_dump($name); 
+
+// no need let or var before declaration
+
+$value1 = $value2 = $value3 = $value4 = $value5 = "zero";
+
+var_dump($float);
+var_dump($value1);
