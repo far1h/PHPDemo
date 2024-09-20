4fccdd4a8ec7c1792db397ecf05f8a05018243a4
far1h <farihmuhammad45@gmail.com>
2024-09-20 08:49:59 +0700

    Add if statements

---
+++ basics/if_statements.php
@@ -0,0 +1,24 @@
+<?php
+
+$number1 = 10;
+$number2 = 20;
+
+
+if($number1 > $number2){ 
+    echo "IT IS ";
+    
+} else {
+    echo "ITS IS NOT ";
+}
+
+echo "<br>";
+
+echo"NEXT LINE"; 
+echo "<br><span>HELLO</span>
+<br>";
+
+?>
+
+<span>HELLO</span>
+<br>
+<span>HELLO</span>
