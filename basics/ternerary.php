af3a0f46cb5d848dc931a2865c2e6e637a0f72f0
far1h <farihmuhammad45@gmail.com>
2024-09-20 15:41:59 +0700

    Add ternerary operators

---
+++ basics/ternerary.php
@@ -0,0 +1,14 @@
+<?php
+
+$is_admin = false;
+
+echo $is_admin ?"Welcome, Admin!":"Access denied";
+
+$is_logged_in = true;
+$message = ($is_logged_in) ? "Welcome back":"Please login";
+
+$age = 18;
+
+$status = ($age >= 18) ?"Adult":"Minor";
+
+echo $status;
