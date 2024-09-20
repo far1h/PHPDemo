609e7d9a67adf321a0dfe3e9e0d7c7486d47f6c8
far1h <farihmuhammad45@gmail.com>
2024-09-20 08:27:45 +0700

    Add typecasting

--- basics/types.php
+++ basics/types.php
@@ -1,7 +1,23 @@
 <?php
 
-var_dump(5);
-var_dump(5.5);
-var_dump(false);
-var_dump([2,"string",1.1]);
-var_dump(null);
+$a = (string) 5; // Integer
+$b = (string) 5.5; // float
+$c = (int) "five"; // string
+$d = (string) false; // boolean
+$e = (object) ["name"=> "Edwin"]; // array
+$f = (string) NULL; // NULL
+
+// var_dump($a);
+// echo "<br>";
+// var_dump ($b);
+// echo "<br>";
+// var_dump ($c);
+// echo "<br>";
+// var_dump ($d);
+// echo "<br>";
+
+echo $e->name;
+
+$e = ["name"=> "Edwin"]; // array
+
+echo $e["name"];
