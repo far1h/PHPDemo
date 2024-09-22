d81db33aa7c3b8e1447085ecb3e654d40ee10c56
far1h <farihmuhammad45@gmail.com>
2024-09-22 08:25:01 +0700

    Add for each loops

--- basics/loops.php
+++ basics/loops.php
@@ -4,13 +4,26 @@
 //     echo "Number: $i <br>";
 // }
 
-$i = 1;
-// while ($i <= 5) {
-//     echo"Number: $i <br>";
-//     $i++;
-// }
+// $i = 1;
+// // while ($i <= 5) {
+// //     echo"Number: $i <br>";
+// //     $i++;
+// // }
 
-do {
-    echo "Number: $i <br>";
-    $i++;
-} while ($i <= 5);
+// do {
+//     echo "Number: $i <br>";
+//     $i++;
+// } while ($i <= 5);
+
+$colors = ["red", "green", "blue"];
+
+foreach ($colors as $color) {
+    echo "". $color ."<br>";
+}
+
+// associative array
+$person = ["name" => "Edwin", "age" => 23];
+
+foreach ($person as $key => $value) {
+    echo "". $key .": ". $value ."<br>";
+}
