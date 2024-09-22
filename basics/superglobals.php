fd70d501c57b108f80150897470bba68c4e1d998
far1h <farihmuhammad45@gmail.com>
2024-09-22 14:31:58 +0700

    Add global variables

---
+++ basics/superglobals.php
@@ -0,0 +1,16 @@
+<?php
+
+// $GLOBALS
+// $_SERVER
+// $_POST
+// $_GET
+// $_COOKIE
+// $_FILES
+
+$app = "PHP CMS";
+$app2 = "JS CMS";
+
+function exampleFunction(){
+    echo $GLOBALS['app'];
+}
+
