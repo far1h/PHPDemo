5be346b9fc98b24a59d5314b291586d14f87d37a
far1h <farihmuhammad45@gmail.com>
2024-09-22 08:21:31 +0700

    Add while and do while loops

--- basics/loops.php
+++ basics/loops.php
@@ -1,5 +1,16 @@
 <?php
 
-for ($i = 0; $i < 5; $i++) {
+// for ($i = 0; $i < 5; $i++) {
+//     echo "Number: $i <br>";
+// }
+
+$i = 1;
+// while ($i <= 5) {
+//     echo"Number: $i <br>";
+//     $i++;
+// }
+
+do {
     echo "Number: $i <br>";
-}
+    $i++;
+} while ($i <= 5);
