e1915e4c45376c3ad6e6506ccd41d4455ba89505
far1h <farihmuhammad45@gmail.com>
2024-09-22 14:37:47 +0700

    Add get requests and other super global variables

--- basics/superglobals.php
+++ basics/superglobals.php
@@ -7,10 +7,21 @@
 // $_COOKIE
 // $_FILES
 
-$app = "PHP CMS";
-$app2 = "JS CMS";
+// $app = "PHP CMS";
+// $app2 = "JS CMS";
 
-function exampleFunction(){
-    echo $GLOBALS['app'];
-}
+// function exampleFunction(){
+//     echo $GLOBALS['app'];
+// }
 
+// GET STUFF FROM THE URL
+// .../superglobals.php/?name=mo&age=19
+$name = $_GET["name"];
+$age = $_GET["age"];
+$status = $_GET["status"];
+
+echo"".$name."<br>".$age."<br>".$status."<br>";
+
+echo $_SERVER["SERVER_NAME"] . "<br>";
+echo $_SERVER["PHP_SELF"] . "<br>";
+echo $_SERVER["HTTP_USER_AGENT"] . "<br>";
