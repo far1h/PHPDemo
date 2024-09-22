3511bc590d5da0ccef0aa0a59fdf61be6ced8956
far1h <farihmuhammad45@gmail.com>
2024-09-22 14:41:42 +0700

    Add built in math functions

---
+++ basics/built-in-math-functions.php
@@ -0,0 +1,22 @@
+<?php
+
+echo abs(-10) ."<br>";
+
+echo round(3.567,2) ."<br>";
+
+echo ceil(4.2) ."<br>";
+
+echo floor(4.2) ."<br>";   
+
+$numbers = [1,2,4,2,9];
+
+echo max($numbers) . "<br>";
+
+echo min($numbers) ."<br>";
+
+echo pow(10,2) ."<br>";
+
+echo sqrt(pow(10,2)) ."<br>";
+
+echo rand(1,100) ."<br>";
+
