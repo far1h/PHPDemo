1cbffcb1dcfd98c6ab451c790c5ba35a95310f1c
far1h <farihmuhammad45@gmail.com>
2024-09-22 14:47:10 +0700

    Add built in string functions

---
+++ basics/built-in-string-functions.php
@@ -0,0 +1,15 @@
+<?php
+
+$str = "Hello, World!";
+
+echo strlen($str) . "<br>"; 
+
+echo strtoupper($str) . "<br>";
+
+echo strtolower($str) . "<br>";
+
+echo strpos($str,"World") . "<br>";
+
+echo str_replace("World","Student", $str) . "<br>";
+
+
