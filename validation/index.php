16c9094641c9128befba338b7c13c23f96e81f5c
far1h <farihmuhammad45@gmail.com>
2024-09-22 16:35:01 +0700

    Add validation part 2

--- validation/index.php
+++ validation/index.php
@@ -9,6 +9,7 @@
     <form action="process.php" method="post">
         <input type="text" placeholder="Enter Username" name="username"> 
         <input type="text" placeholder="Enter Email" name="email"><br><br>
+        <!-- <input type="email" placeholder="Enter Email" name="email"><br><br> -->
         <input type="submit">
     </form>
 </body>
--- validation/process.php
+++ validation/process.php
@@ -4,10 +4,16 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     $usernameErr = $emailErr = "";
     $username = $email = "";
 
-    if(empty($_POST["username"])){
+    if(empty($_POST["username"]) || empty($_POST["email"])){
         $usernameErr = "username is required";
+        $emailErr = "email is required";
     } else {
         $username = htmlspecialchars(trim($_POST["username"]));
+        $email = htmlspecialchars(trim($_POST["email"]));
+
+        if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+            $emailErr = "Invalid email format";
+        }
     }
 }
 
