2b65379d5a6f191e0fec264cafbfcd6a054b8d53
far1h <farihmuhammad45@gmail.com>
2024-09-22 15:54:51 +0700

    Add basic forms

---
+++ forms/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Simple Form</title>
+</head>
+<body>
+    <h2>Basic Form</h2>
+
+    <form action="process.php" method="get">
+        <input type="text" placeholder="Enter Username" name="username"> 
+        <input type="text" placeholder="Enter Password" name="password">
+        <input type="text" placeholder="Enter Status" name="status">
+        <input type="submit">
+    </form>
+</body>
+</html>
