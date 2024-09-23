69b8399ca169376d8263d6d1c4ea7d26f7e6eba8
far1h <farihmuhammad45@gmail.com>
2024-09-23 08:51:43 +0700

    Add reading files easy method

--- handling-files/index.php
+++ handling-files/index.php
@@ -1,15 +1,19 @@
 <?php
 
-$file_name = "data.txt";
+// $file_name = "data.txt";
 
-$file = fopen($file_name,"r");
+// $file = fopen($file_name,"r");
 
-// var_dump($file);
+// // var_dump($file);
 
-if ($file) {
-    $content = fread($file, filesize($file_name));
-    echo nl2br($content);
-    fclose($file);
-} else {
-    echo "Unable to open file";
-}
+// if ($file) {
+//     $content = fread($file, filesize($file_name));
+//     echo nl2br($content);
+//     fclose($file);
+// } else {
+//     echo "Unable to open file";
+// }
+
+// just to echo content
+$content = file_get_contents("data.txt");
+echo nl2br($content);
