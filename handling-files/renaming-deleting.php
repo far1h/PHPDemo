8b2d69cba063c945ef22bbaac2c2128c218036bb
far1h <farihmuhammad45@gmail.com>
2024-09-23 09:13:19 +0700

    Add deleting files

--- handling-files/renaming-deleting.php
+++ handling-files/renaming-deleting.php
@@ -3,10 +3,16 @@
 $old_name = "data.txt";
 $new_name = "new_data.txt";
 
-if(file_exists($old_name)){
-    rename($old_name, $new_name);
-    echo "File renamed";
+// if(file_exists($old_name)){
+//     rename($old_name, $new_name);
+//     echo "File renamed";
+// } else {
+//     echo "File not found";
+// }
+
+if(file_exists($new_name)){
+    unlink($new_name);
+    echo "File deleted";
 } else {
     echo "File not found";
 }
-
