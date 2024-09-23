23db622f97c46db09f413577572b203f6ef23473
far1h <farihmuhammad45@gmail.com>
2024-09-23 09:24:14 +0700

    Create form for file upload

---
+++ uploading/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>File Uploading</title>
+</head>
+<body>
+    <h2>Upload a file</h2>
+
+    <form action="upload.php"></form>
+    Select file : <input type="file" name="file"><br><br>
+    <input type="submit" value="Upload File">
+</body>
+</html>
