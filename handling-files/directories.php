8a07dccd68fa76a40a7b6d1d9b94e804c27ed954
far1h <farihmuhammad45@gmail.com>
2024-09-23 09:20:15 +0700

    Add scanning from directory

--- handling-files/directories.php
+++ handling-files/directories.php
@@ -2,10 +2,23 @@
 
 $directory = "uploads";
 
-// Creating directory
-if(!file_exists($directory)){  
-    mkdir($directory,0777, true);
-    echo "Directory created";
-} else {
-    echo "Directory already exists";
+// scan directory
+if(is_dir($directory)){
+    $files = scandir($directory);
+    foreach($files as $file){
+        if($file !=="."&& $file !== ".."){
+            echo "File: $file <br>";
+        }
+    }
 }
+
+
+
+
+// Creating directory
+// if(!file_exists($directory)){  
+//     mkdir($directory,0777, true);
+//     echo "Directory created";
+// } else {
+//     echo "Directory already exists";
+// }
