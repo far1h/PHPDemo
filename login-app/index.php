7f177bd72520bbb47de2dd568b1faf4f86651da3
far1h <farihmuhammad45@gmail.com>
2024-09-27 11:54:34 +0700

    Add navigation styling

--- login-app/index.php
+++ login-app/index.php
@@ -1,21 +1,9 @@
 <?php
 
-include("db.php");
-
 session_start();
-
+include("partials/header.php");
+include("partials/navigation.php");
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login App with SQL and PHP</title>
-</head>
-<body>
-    <h2>Welcome to the homepage</h2>
-    
-    <?php include("partials/navigation.php");   ?>
-   
+<h2>Welcome to the homepage</h2>   
 <?php include("partials/footer.php");   ?>
