db85552ea9b07f7376d3067113981c3a71f779a8
far1h <farihmuhammad45@gmail.com>
2024-09-29 16:19:05 +0700

    Add CSS adjustments

--- todo-app-mysqli-oop/index.php
+++ todo-app-mysqli-oop/index.php
@@ -40,7 +40,7 @@ $tasks = $todo->read();
 ?>
 <!-- Notification Container -->
 <?php if(isset($_SESSION['message'])): ?>
-<div class="notification-container">
+<div class="notification-container <?php echo isset($_SESSION['message']) ? 'show' : ''?>">
     <div class="notification <?php echo $_SESSION['msg_type']?>">
         <?php echo $_SESSION['message']; ?>
         <?php unset($_SESSION['message']); ?>
