366de45befbff7c5837fe2d76a37ffb4414fd488
far1h <farihmuhammad45@gmail.com>
2024-09-29 13:38:51 +0700

    Try static methods and functions

--- OOP/class_declaration.php
+++ OOP/class_declaration.php
@@ -1,101 +1,19 @@
 <?php
 
 class User {
-    public $name;
-    public $email;
+    public static $userCount = 0;
 
-    public function __construct($name, $email) {
-        $this->name = $name;
-        $this->email = $email;
+    public function __construct() {
+        self::$userCount++;
     }
-
-    public function displayUserInfo(){
-        echo"User: ".$this->name.", Email:" .$this->email;
-    }
-
-    /**
-     * Get the value of email
-     */ 
-    public function getEmail()
-    {
-        return $this->email;
-    }
-
-    /**
-     * Set the value of email
-     *
-     * @return  self
-     */ 
-    public function setEmail($email)
-    {
-        $this->email = $email;
-
-        return $this;
-    }
-
-    /**
-     * Get the value of name
-     */ 
-    public function getName()
-    {
-        return $this->name;
-    }
-
-    /**
-     * Set the value of name
-     *
-     * @return  self
-     */ 
-    public function setName($name)
-    {
-        $this->name = $name;
-
-        return $this;
+    public static function getUserCount() {
+        return "Total users: " . self::$userCount;
     }
 }
 
-class AdminUser extends User {
-    public $role;
+$user = new User();
+$user = new User();
+$user = new User();
+$user = new User();
 
-    public function __construct($name, $email, $role) {
-        parent::__construct($name, $email);
-        $this->role = $role;
-    }
-
-    public function displayUserInfo(){
-        parent::displayUserInfo();
-        echo ", Role: ".$this->role;
-    }
-
-    /**
-     * Get the value of role
-     */ 
-    public function getRole()
-    {
-        return $this->role;
-    }
-
-    /**
-     * Set the value of role
-     *
-     * @return  self
-     */ 
-    public function setRole($role)
-    {
-        $this->role = $role;
-
-        return $this;
-    }
-}
-
-$user = new User("mo", "mo@123.com");
-
-$user->displayUserInfo();
-
-echo "<br>";
-
-$admin = new AdminUser("admin","admin@123.com","Admin");
-
-$admin->displayUserInfo();
-
-echo "<br>";
+echo User::getUserCount();
