c80d2d9374849368d9f00a0be1e8e5147b64ca05
far1h <farihmuhammad45@gmail.com>
2024-09-29 12:11:00 +0700

    Add SQL injection problem

--- sql-injection/index.php
+++ sql-injection/index.php
@@ -2,6 +2,18 @@
 
 $conn = mysqli_connect("localhost","root","","sql_injection");
 
-if($conn){
-    echo "Connected";
-}
+$query = "SELECT * FROM users WHERE username = 'edwin' OR '1'='1'";
+
+$result = mysqli_query($conn, $query);
+
+var_dump($result);
+
+?>
+
+<ul>
+    <?php while($user = mysqli_fetch_assoc( $result )) : ?>
+        <li>
+            <?php echo $user["username"]; ?>
+        </li>
+    <?php endwhile; ?>
+</ul>
