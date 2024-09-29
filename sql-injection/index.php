444b9eecb13270e7a11cfd78014f6d059940c3d1
far1h <farihmuhammad45@gmail.com>
2024-09-29 12:17:25 +0700

    Add SQL injection solution

--- sql-injection/index.php
+++ sql-injection/index.php
@@ -1,12 +1,27 @@
 <?php
 
+$username = 'john_doe';
 $conn = mysqli_connect("localhost","root","","sql_injection");
 
-$query = "SELECT * FROM users WHERE username = 'edwin' OR '1'='1'";
+$query = "SELECT * FROM users WHERE username = '$username' OR '1'='1'";
 
 $result = mysqli_query($conn, $query);
 
-var_dump($result);
+// AVOID SQL INJECTION with prepared stmts
+
+$query = "SELECT * FROM users WHERE username = ?";
+
+$stmt = mysqli_prepare($conn, $query);
+
+// Bind the param
+
+mysqli_stmt_bind_param($stmt,"s", $username);
+
+// execute
+
+mysqli_stmt_execute($stmt);
+
+$result = mysqli_stmt_get_result($stmt);
 
 ?>
 
